f4c2582090f75de754e087e91d7f9acbec8ff2bf
iyaki <ivanyaki96@gmail.com>
2020-11-15 21:50:24 -0300

    Added time range rule

--- schedule.go
+++ schedule.go
@@ -1,26 +1,45 @@
 package schedule
 
 import (
+	"errors"
 	"time"
 )
 
 // Schedule to store and retrieve appointments
 type Schedule struct {
-	appointments []appointment
+	StartTime    time.Time
+	EndTime      time.Time
+	appointments []schedulable
 }
 
-type appointment struct {
-	Datetime time.Time
+type schedulable interface {
+	Datetime() time.Time
 }
+
+// ErrOutOfTimeRange is the error given when try to make an appointmenrt out of the schedule range
+var ErrOutOfTimeRange = errors.New("The given time for the appointment is out of the range defined in the schedule")
 
 // NewAppointment add an appointment to the schedule if possible
-func (s *Schedule) NewAppointment(dateTime time.Time) appointment {
-	app := appointment{Datetime: dateTime}
-	s.appointments = append(s.appointments, app)
-	return app
+func (s *Schedule) NewAppointment(dateTime time.Time) (schedulable, error) {
+	if dateTime.Format("15:04:05") < s.StartTime.Format("15:04:05") ||
+		dateTime.Format("15:04:05") > s.EndTime.Format("15:04:05") {
+		return nil, ErrOutOfTimeRange
+	}
+
+	newAppointment := appointment{datetime: dateTime}
+	s.appointments = append(s.appointments, newAppointment)
+	return newAppointment, nil
 }
 
 // Appointments returns the existing appointments of the schedule
-func (s *Schedule) Appointments() []appointment {
+func (s *Schedule) Appointments() []schedulable {
 	return s.appointments
+}
+
+type appointment struct {
+	datetime time.Time
+}
+
+func (a appointment) Datetime() time.Time {
+	return a.datetime
 }
--- schedule_test.go
+++ schedule_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestAppointments(t *testing.T) {
 	testAppointment := appointment{}
-	schedule := Schedule{appointments: []appointment{testAppointment}}
+	schedule := Schedule{appointments: []schedulable{testAppointment}}
 
 	got := schedule.Appointments()[0]
 
@@ -15,17 +15,67 @@ func TestAppointments(t *testing.T) {
 }
 
 func TestNewAppointment(t *testing.T) {
-	schedule := Schedule{}
+	t.Run("Add new valid appointment to a schedule", func(t *testing.T) {
+		startTime := time.Date(0, 0, 0, 8, 0, 0, 0, time.UTC)
+		endTime := time.Date(0, 0, 0, 16, 0, 0, 0, time.UTC)
+		appointmentDateTime := time.Date(0, 0, 0, 9, 30, 0, 0, time.UTC)
+		schedule := Schedule{
+			StartTime: startTime,
+			EndTime:   endTime,
+		}
 
-	newAppointment := schedule.NewAppointment(time.Now())
-	gettedAppointment := schedule.Appointments()[0]
+		newAppointment, err := schedule.NewAppointment(appointmentDateTime)
 
-	assertEqual(t, newAppointment, gettedAppointment)
+		appointments := schedule.Appointments()
+		appointmentsAmmount := len(appointments)
+		gettedAppointment := appointments[0]
+
+		assertNoError(t, err)
+		assertEqual(t, appointmentDateTime, gettedAppointment.Datetime())
+		assertEqual(t, newAppointment, gettedAppointment)
+		assertEqual(t, appointmentsAmmount, 1)
+	})
+
+	t.Run("Add new appointment to a schedule out of hour range", func(t *testing.T) {
+		startTime := time.Date(0, 0, 0, 8, 0, 0, 0, time.UTC)
+		endTime := time.Date(0, 0, 0, 16, 0, 0, 0, time.UTC)
+		appointmentDateTime := time.Date(0, 0, 0, 6, 30, 0, 0, time.UTC)
+		schedule := Schedule{
+			StartTime: startTime,
+			EndTime:   endTime,
+		}
+
+		newAppointment, err := schedule.NewAppointment(appointmentDateTime)
+
+		appointmentsAmmount := len(schedule.Appointments())
+
+		assertError(t, err, ErrOutOfTimeRange)
+		assertEqual(t, newAppointment, nil)
+		assertEqual(t, appointmentsAmmount, 0)
+	})
 }
 
 func assertEqual(t *testing.T, expected, got interface{}) {
 	t.Helper()
 	if expected != got {
 		t.Errorf("expected %v but got %v", expected, got)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("getted an error but didn't want one")
 	}
 }
